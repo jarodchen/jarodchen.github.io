2efec73cb723a057418b4311e4870f777cd51893
CJ <hljchenjun@live.com>
2026-08-06 00:17:20 +0800

    doc: 单例模式

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'vitepress'
 import { withMermaid } from 'vitepress-plugin-mermaid'
 import { generateBlogSidebar, updateArchivesPage, updateBlogIndexPage } from './blog-utils'
 import { RssPlugin } from 'vitepress-plugin-rss'
+import { BiDirectionalLinks } from '@nolebase/markdown-it-bi-directional-links' // [!code ++]
+
 
 // 启动时自动生成博客首页和归档页面（仅执行一次）
 updateBlogIndexPage()
@@ -92,6 +94,12 @@ export default withMermaid(defineConfig({
     theme: {
       light: 'github-light',
       dark: 'github-dark'
+    },
+    config: (md) => {
+      md.use(BiDirectionalLinks({
+        dir: './docs',          // 链接解析的根目录，默认文档根目录
+        includesPatterns: ['**/*.md'] // 匹配文件模式
+      }) as any)
     }
   },
   
